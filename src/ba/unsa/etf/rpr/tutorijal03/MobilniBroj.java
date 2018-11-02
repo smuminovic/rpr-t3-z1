701a1a28b68e25368e9d6f1b9bbad27565075bd4
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-11-02 15:02:49 +0100

    Konstrukor klase MobilniBroj, i metode setMobilnaMreza i setBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -2,8 +2,20 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class MobilniBroj extends TelefonskiBroj {
     //atributi
-    int mobilnaMreza;
-    String broj;
+    private int mobilnaMreza;
+    private String broj;
+    //konstruktor
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        this.setMobilnaMreza(mobilnaMreza);
+        this.setBroj(broj);
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
+    public void setMobilnaMreza(int mobilnaMreza) {
+        this.mobilnaMreza = mobilnaMreza;
+    }
 
     public int getMobilnaMreza() {
         return mobilnaMreza;
