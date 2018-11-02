d7ba0708299a80db516fb937089b27996f56e3c8
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-11-02 14:50:36 +0100

    initial commit

---
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import java.util.HashMap;
+
+public abstract class TelefonskiBroj {
+    public abstract String ispisi();
+    public abstract int hashCode();
+
+}
